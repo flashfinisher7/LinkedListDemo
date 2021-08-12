3fc78be68542ba761ac651b16f8417cbd591a592
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-12 09:25:21 +0530

    Refactor: LinkedList Demo Problem

---
+++ DataStructureDemo/DataStructureDemo/LinkedList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureDemo
+{
+    public class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Inserted Into Linked List ", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+                return;
+            }
+            while(temp !=null)
+            {
+                Console.WriteLine(temp.data+ "");
+                temp = temp.next;
+            }
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid Position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("position out of range");
+            }
+            Console.WriteLine("Insterted Line Value is " + data);
+            return head;
+        }
+    }
+}
---
+++ DataStructureDemo/DataStructureDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LinkedList Demo Problem");
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            //List.Add(67);
+            List.Add(70);
+            List.Display();
+            List.InsertAtParticularPosition(2, 30);
+        }
+    }
+}
